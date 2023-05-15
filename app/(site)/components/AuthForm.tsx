8ec4048004c85d8abf9ef32f14d7f1d40f6fc91d
mausmi00 <mausmi000@gmail.com>
2023-05-15 15:05:35 +0400

    impl auth UI

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,54 +1,163 @@
-"use client"; //this makes the auth form intercative (buttons etc are not compatible with server components)
+"use client"; //this makes the auth form interactive (buttons etc are not compatible with server components)
 
+import Button from "@/app/components/Buttons";
+import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-
-type Variant = 'LOGIN' | 'REGISTER';
+import AuthSocialButton from "./AuthSocialButton";
+import {BsGithub, BsGoogle} from 'react-icons/bs'
+type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
-    const [variant, setVariant] = useState<Variant>('LOGIN');
-    const [isLoading, setIsLoding] = useState(false); // to disable buttons and inputs once the form is submitted
-  
-    const toggleVariant = useCallback(() => {
-        if (variant == 'LOGIN') {
-            setVariant('REGISTER');
-        } else {
-            setVariant('LOGIN');
-        }
-    }, [variant]);
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoding] = useState(false); // to disable buttons and inputs once the form is submitted
 
-    // const {extract functions}
-    const {
-        register, 
-        handleSubmit, 
-        formState: {
-            errors
-        }
-    } = useForm<FieldValues>({
-        defaultValues: {
-            name: '',
-            email: '',
-            password: ''
-        }
-    });
+  const toggleVariant = useCallback(() => {
+    if (variant == "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
 
-    const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        setIsLoding(true);
-        if(variant == 'REGISTER') {
-            // Axios Register
-        }
+  // const {extract functions}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
 
-        if(variant == 'LOGIN') {
-            // NextAuth SignIn
-        }
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoding(true);
+    if (variant == "REGISTER") {
+      // Axios Register
     }
 
-    const socialAction = (action: string) => {
-        setIsLoding(true);
-        // NextAuth Social SignIn
+    if (variant == "LOGIN") {
+      // NextAuth SignIn
     }
-  
-    return <div>hello2222222222222234</div>;
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoding(true);
+    // NextAuth Social SignIn
+  };
+
+  return (
+    <div
+      className="
+     mt-8
+    sm:mx-auto
+    sm:w-full
+    sm:max-w-md"
+    >
+        <div className="
+        bg-white
+        px-4
+        py-8
+        shadow
+        sm:rounded-lg
+        sm:px-10">
+            <form
+            /* Here, handleSubmit comes from the extracted function useForm and 
+            the wrapped onSubmit is the function that we wrote above.
+            The reason we use the handleSumbit wrapper is because we need to provide
+            the onSubmit the "data" which is passed on by handleSubmit*/
+            className="space-y-6" 
+            onSubmit={handleSubmit(onSubmit)}
+            >
+                {variant === 'REGISTER' && (
+                    <Input 
+                    id="name" 
+                label="Name" 
+                register={register}
+                errors={errors}
+                disabled={isLoading}
+                />
+                
+                )}
+                <Input
+                id="email" 
+                label="Email" 
+                type="email"
+                register={register}
+                errors={errors}
+                disabled={isLoading}
+                />
+
+                <Input 
+                id="password" 
+                label="Password" 
+                register={register}
+                errors={errors}
+                disabled={isLoading}
+                />
+                <div>
+                    <Button /* We don't need onClick here since we have already defined onSubmit above in the form component */
+                    disabled={isLoading}
+                    fullWidth
+                    type='submit'>{variant == 'LOGIN' ? 'Sign In' : 'Register'}</Button>
+                    
+                </div>                  
+            </form>
+            <div className="mt-6">
+                <div className="relative">
+                    <div className="
+                    absolute
+                    inset-0
+                    flex
+                    items-center
+                    ">
+                        <div className="
+                        w-full
+                        border-t
+                        border-gray-300"/>
+                    </div>
+                    <div className="relative
+                     flex
+                      justify-center
+                       text-sm">
+                        <span className="bg-white 
+                        px-2
+                         text-gray-500">
+                            Or continue with    
+                         </span>
+                    </div>
+                </div>
+                <div className="mt-6 flex gap-2">
+                    <AuthSocialButton
+                    icon={BsGithub}
+                    onClick={() => socialAction('github')}/>
+                    <AuthSocialButton
+                    icon={BsGoogle}
+                    onClick={() => socialAction('google')}/>
+                </div>
+            </div>
+            <div className="
+            flex
+            gap-2
+            justify-center
+            text-sm
+            mt-6
+            px-2
+            text-gray-500">
+                <div>
+                    {variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'}
+                </div>
+                <div onClick={toggleVariant}
+                className="underline cursor-pointer">
+                    {variant === 'LOGIN' ? 'Create an account' : 'Login '}
+                </div>                
+            </div>
+        </div>
+    </div>
+  );
 };
 
 export default AuthForm;
---
+++ app/components/inputs/Input.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import clsx from "clsx";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  label: string;
+  id: string;
+  type?: string;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+  disabled?: boolean;
+}
+
+const Input: React.FC<InputProps> = ({
+  label,
+  id,
+  type,
+  required,
+  register,
+  errors,
+  disabled,
+}) => {
+  return (
+    <div>
+      <label
+        className="
+        block
+        text-sm
+        font-medium
+        leading-6
+        text-grey-900
+        "
+        htmlFor={id}
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+        id={id}
+        type={type}
+        autocomplete={id}
+        disabled={disabled} 
+        {...register(id, {required})} /*register handles actions like onChange, onFocus and a bunch of other functions */
+        /* clsx: allows us to dynamically use classes */
+        className={clsx(`
+        form-input
+        block
+        w-full
+        rounded-md
+        border-0
+        py-1.5
+        text-gray-900
+        shadow-sm
+        ring-1
+        ring-inset
+        ring-gray-300
+        placeholder:text-gray-400
+        focus:ring-2
+        focus:ring-inset
+        focus:ring-sky-600
+        sm:text-sm
+        sm:leading-6`,
+        errors[id] && "focus:ring-rose-500", /* if errors has an id then it will be displayed in red */
+        disabled && "opactity-50 cursor-default")}
+        />
+
+      </div>
+    </div>
+  );
+};
+
+export default Input;
