345eddf3771748033a5e577370adee804f0958c1
mausmi00 <mausmi000@gmail.com>
2023-05-17 16:20:30 +0400

    created route/converstaion hooks and impl sidebar

---
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+const DesktopSidebar = () => {
+    return (  <div className="
+    hidden
+    lg:fixed
+    lg:inset-y-0
+    lg:left-0
+    lg:z-40
+    lg:w-20
+    lg:px-6
+    lg:overflow-y-auto
+    lg:bg-white
+    lg:border-r-[1px]
+    lg:pb-4
+    lg:flex
+    lg:flex-col
+    justify-between">
+        <nav className="
+        mt-4
+        flex
+        flex-col
+        justify-between">
+            <ul role="list"
+            className="flex
+            flex-col
+            items-center
+            space-y-1
+            ">
+                
+            </ul>
+
+        </nav>
+        DesktopSidebar!
+    </div>);
+}
+ 
+export default DesktopSidebar;
