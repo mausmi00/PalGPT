18f285fbc3753e7c51082c07a18ee709c3adbbb4
mausmi00 <mausmi000@gmail.com>
2023-06-27 00:29:21 +0530

    commented index.ts

--- index.ts
+++ index.ts
@@ -1,69 +1,69 @@
-import { PrismaClient, Prisma } from '@prisma/client'
+// import { PrismaClient, Prisma } from '@prisma/client'
 
-const prisma = new PrismaClient()
-async function main() {
-    // const user = await prisma.user.update({
-    //     where: {
-    //         id: "648343977f71596b4b97abef"
-    //     },
-    //     data: {
-    //         seenMessageIds: []
-    //     }
-    // })
+// const prisma = new PrismaClient()
+// async function main() {
+//     // const user = await prisma.user.update({
+//     //     where: {
+//     //         id: "648343977f71596b4b97abef"
+//     //     },
+//     //     data: {
+//     //         seenMessageIds: []
+//     //     }
+//     // })
 
-    // const createUser = await prisma.user.create({
-    //     data: {
-    //         name: "Socrates",
-    //         email: "socrates@ai.com",
-    //         image: "/images/socrates.jpg",
-    //         createdAt: new Date(),
-    //         updatedAt: new Date(),
-    //         isAi: true,
-    //         characteristics: "Greek philosopher"
-    //     }
-    // })
+//     // const createUser = await prisma.user.create({
+//     //     data: {
+//     //         name: "Socrates",
+//     //         email: "socrates@ai.com",
+//     //         image: "/images/socrates.jpg",
+//     //         createdAt: new Date(),
+//     //         updatedAt: new Date(),
+//     //         isAi: true,
+//     //         characteristics: "Greek philosopher"
+//     //     }
+//     // })
 
-    const updatedUser = await prisma.user.update({
-        where: {
-            id: "6496f832f91b008949a2f483",
-        },
-        data: {
-            characteristics: "I'm someone who feels deeply and isn't afraid to let my emotions show through tears"
-        }
-    })
+//     const updatedUser = await prisma.user.update({
+//         where: {
+//             id: "6496f832f91b008949a2f483",
+//         },
+//         data: {
+//             characteristics: "I'm someone who feels deeply and isn't afraid to let my emotions show through tears"
+//         }
+//     })
 
-    // const user = await prisma.user.update({
-    //     where: {
-    //         id: "649222ef4678e55bf8e39998"
-    //     },
-    //     data: {
-    //         seenMessageIds: []
-    //     }
-    // })
+//     // const user = await prisma.user.update({
+//     //     where: {
+//     //         id: "649222ef4678e55bf8e39998"
+//     //     },
+//     //     data: {
+//     //         seenMessageIds: []
+//     //     }
+//     // })
 
 
-    // const conv = await prisma.user.updateMany({
-    //     data: {
-    //         conversationIds: [],
-    //         seenMessageIds: [],
-    //     }
-    // })
+//     // const conv = await prisma.user.updateMany({
+//     //     data: {
+//     //         conversationIds: [],
+//     //         seenMessageIds: [],
+//     //     }
+//     // })
 
-    // const user = await prisma.user.update({
-    //     where: {
-    //         id: "649221204678e55bf8e3998c"
-    //     },
-    //     data: {
-    //         image: "/images/groot.jpg"
-    //     }
-    // })
-}
-main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+//     // const user = await prisma.user.update({
+//     //     where: {
+//     //         id: "649221204678e55bf8e3998c"
+//     //     },
+//     //     data: {
+//     //         image: "/images/groot.jpg"
+//     //     }
+//     // })
+// }
+// main()
+//     .then(async () => {
+//         await prisma.$disconnect()
+//     })
+//     .catch(async (e) => {
+//         console.error(e)
+//         await prisma.$disconnect()
+//         process.exit(1)
+//     })
