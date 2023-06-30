76656aed92cb84707acc316764353be193b0b3e5
mausmi00 <mausmi000@gmail.com>
2023-07-01 02:35:55 +0530

    fixed

--- app/components/sidebar/SettingsModal.tsx
+++ app/components/sidebar/SettingsModal.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { User } from "@prisma/client";
-
-
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import axios from "axios";
